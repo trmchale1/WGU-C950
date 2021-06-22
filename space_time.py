7bb7b54ffbf614fb753a2dd02336e094a2386474
Tim McHale <timmchale@Tims-MacBook-Pro.local>
2021-06-21 20:16:12 -0500

    sorted bus 2

--- space_time.py
+++ space_time.py
@@ -13,29 +13,28 @@ first_delivery_sorted = []
 temp = []
 ind = []
 
+print(second_delivery)
+
 def greedy_func(temp_list):
     temp = []
     temp_list.sort(key = lambda x: float(x[2]))
-    print(temp_list)
-    temp.append(temp_list[0])
+    if len(temp_list) == 0:
+        return
+    else:
+        temp.append(temp_list[0])
     x = temp[0][2]
     for i in range(len(temp_list)):
         if temp_list[i][2] == x and temp_list[i] not in temp:
             temp.append(temp_list[i])        
-    print('Subgraph: ')
-    print(temp)
     return temp
     
 node = 'HUB'
 while len(first_delivery) != 0:
-
     for p in first_delivery:
         for e in edges:
             if e[1].strip() == node and pack[p]["address"] == e[0] and float(e[2]) > 0.0 and p not in first_delivery_sorted:
-                print("Package number : " + str(p) + " Current Location : " + node + " loc: " + e[0] + " loc: " + e[1] + " dist: " + e[2])
                 temp.append((p,e[0],e[2]))
             elif e[0].strip() == node and pack[p]["address"] == e[1] and float(e[2]) > 0.0 and p not in first_delivery_sorted:
-                print("Package number : " + str(p) + " Current Location : " + node + " loc: " + e[0] + " loc: " + e[1] + " dist: " + e[2])
                 temp.append((p,e[1],e[2]))
     min_pair = greedy_func(temp)
     if isinstance(min_pair, list):
@@ -45,20 +44,28 @@ while len(first_delivery) != 0:
             if i[0] in first_delivery:
                 first_delivery.remove(i[0])
     temp.clear()
-    print('Final List: ')
-    print(first_delivery_sorted)
 
-'''   
-print(first_delivery)
-print(first_delivery_sorted)
-temp.clear()    
-count = 0
-# iterate over edges to match the next first_delivery index to build a list, then find the min, the next destination, then add to the sorted list
-for i in first_delivery_sorted:
-    for e in edges:
-        if (i[1] == e[0] or i[1] == e[1]) and e[2] > i[2]:
-            print(count)
-            print(i,e[0],e[1],e[2])
-            count = count + 1
-             and e[1].strip() == node
-'''
+
+temp.clear()
+second_delivery_sorted = []    
+print(second_delivery_sorted)
+node = 'HUB'
+while len(second_delivery) != 0:
+    for p in second_delivery:
+        for e in edges:
+            if e[1].strip() == node and pack[p]["address"] == e[0] and float(e[2]) > 0.0 and p not in second_delivery_sorted:
+                print("Package number : " + str(p) + " Current Location : " + node + " loc: " + e[0] + " loc: " + e[1] + " dist: " + e[2])
+                temp.append((p,e[0],e[2]))
+            elif e[0].strip() == node and pack[p]["address"] == e[1] and float(e[2]) > 0.0 and p not in second_delivery_sorted:
+                print("Package number : " + str(p) + " Current Location : " + node + " loc: " + e[0] + " loc: " + e[1] + " dist: " + e[2])
+                temp.append((p,e[1],e[2]))
+        minimum_pair = greedy_func(temp)
+    if isinstance(minimum_pair, list):
+        for i in minimum_pair:
+            second_delivery_sorted.append(i)
+            node = i[1]
+            if i[0] in second_delivery:
+                second_delivery.remove(i[0])
+    temp.clear()
+print('Final List: ')
+print(second_delivery_sorted)
